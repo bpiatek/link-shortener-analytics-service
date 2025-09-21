1cee7ae7fb46b79ca59d2be0e53360ea2886a3e2
Bartosz Piatek <your.email@example.com>
2025-09-21 12:21:51 +0200

    first commit

---
+++ src/main/java/pl/bpiatek/linkshorteneranalyticsservice/LinkShortenerAnalyticsServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.bpiatek.linkshorteneranalyticsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkShortenerAnalyticsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkShortenerAnalyticsServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/bpiatek/linkshorteneranalyticsservice/LinkShortenerAnalyticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bpiatek.linkshorteneranalyticsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkShortenerAnalyticsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
